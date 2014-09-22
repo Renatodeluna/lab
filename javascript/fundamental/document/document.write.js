b1cc766c364ac658858c78eb2f0fb503ebbc2e2c
renatodeluna <deluna.developer@gmail.com>
2014-09-22 01:30:51 -0300

    document.write

---
+++ javascript/fundamental/document/document.write.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+// 	------ Write -------------------------------------------------
+	
+	// Syntax
+	document.write(markup);
+
+	
+
+	// String
+	document.write('Hello Word! ;)');
+
+
+
+	// Number
+	document.write(15 + 19); // => 34
+
+
+
+	// HTML 
+	document.write('<h1> Hello Word! </h1>');
+
+
+
+	var ano = 2014;
+	document.write('I was born on: ' + (ano - 34) + '<hr>'); // => I was born on: 1980 
+
+
+
+	// Object
+	document.write(window.innerHeight + 'px');
+
