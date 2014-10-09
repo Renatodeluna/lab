48ae2dce747e39736ebeb709c3b4e4234a43821a
rdeluna <deluna.developer@gmail.com>
2014-10-09 02:36:12 -0300

    window.confirm

--- javascript/fundamental/window/window.confirm.js
+++ javascript/fundamental/window/window.confirm.js
@@ -3,7 +3,23 @@
 
 // 	------ Confirm -------------------------------------------------
 	
-	confirm('Salve?');
+	// Syntax
+	window.confirm(message);
 
 
-	
+	// 'window' not mandatory
+	if(confirm('Do you really want to delete this file?')) {
+		document.write('successfully deleted file!');
+	};
+
+
+
+	var reload = function() {
+
+		if(confirm('you really want to exit this page?')) {
+			location.reload();
+		};
+
+	};
+
+	reload();
