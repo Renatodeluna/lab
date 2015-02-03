261cf8458b01c0a1c57daa2c7ea93c274eed3baa
Renatodeluna <deluna.developer@gmail.com>
2015-02-03 20:13:52 -0300

    arithmetic operators

---
+++ javascript/fundamental/operators/arithmeticOperators.js
@@ -0,0 +1,105 @@
+'use strinct';
+
+//  ==========================================================================
+//  Operators Arithmetic
+//  ==========================================================================
+
+	// Sintax
+	Operator									 Description
+		+		______________________	Addition
+		-		______________________	Subtraction
+		*		______________________	Multiplication
+		/		______________________	Division
+		%		______________________	Modulus
+		++	______________________	Increment
+		--	______________________	Decrement
+
+
+
+	// Addition
+	var x = 5;
+	var y = 2;
+	var w = x + y;	// -> 7
+
+
+	// Subtraction
+	var x = 5;
+	var y = 2;
+	var w = x - y;  // -> 3
+
+
+	//  Multiplication
+	var x = 5;
+	var y = 2;
+	var w = x * y // -> 10
+
+
+	// Division 
+	var x = 5;
+	var y = 2;
+	var w = x / 2;	// -> 2.5	
+	
+
+	//  Modulus
+	var x = 5;
+	var y = 2;
+	var w = x % y; // -> 0
+
+
+	// Increment
+	var x = 5;
+	x++;
+	var w = x;	// -> 6
+
+
+	// Decrement 
+	var x = 5;
+	x--;
+	var w = x;	// -> 4
+
+
+
+	// Assignment Operators
+	Operator									 Exemplo
+		+= ______________________ x += y ______________________ x = x + y
+		-= ______________________ x -= y ______________________ x = x - y
+		*= ______________________ x *= y ______________________ x = x * y
+		/= ______________________ x /= y ______________________ x = x / y
+		%= ______________________ x %= y ______________________ x = x % y
+
+
+
+	// Assignment operator adds
+	var x = 5;
+	x += 2;		// -> 7
+		
+
+	// Assignment operator subtracts
+	var x = 5;
+	x -= 2;		// -> 3
+
+
+	// Assignment operator multiplies
+	var x = 5;
+	x *= 2;		// -> 10
+
+
+	// Assignment divides
+	var x = 5;
+	x /= 2;		// -> 2.5
+
+
+	// Assignment operator assigns a remainder
+	var x = 5;
+	x %= 2;		// -> 1
+		
+		
+	 	 
+		
+
+
+
+
+		
+		
+
