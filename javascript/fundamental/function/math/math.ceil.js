93f67461eb7d5d9fb0d664949f8e8704cd8716ad
Renatodeluna <deluna.developer@gmail.com>
2015-02-05 20:46:14 -0300

    function math

---
+++ javascript/fundamental/function/math/math.ceil.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//  ==========================================================================
+//  Math Ceil
+//  ==========================================================================
+
+	
+
+	// Syntax
+	Math.ceil(x);
+
+
+	//	return 1
+	var a = Math.ceil(0.60);
+
+
+	//	return 1
+	var b = Math.ceil(0.40);
+
+
+	//	return 6
+	var c = Math.ceil(5);
+
+
+	//	return 6
+	var d = Math.ceil(5.1);
+
+
+	//	return -5
+	var e = Math.ceil(-5.1);
+
+
+	//	return -5
+	var f = Math.ceil(-5.9);
--- javascript/fundamental/function/math/math.round.js
+++ javascript/fundamental/function/math/math.round.js
@@ -1,12 +1,34 @@
 'use strict';
 
+//  ==========================================================================
+//  Math Round
+//  ==========================================================================
 
-// 	------ Math Round -------------------------------------------------
+	
 
 	// Syntax
 	Math.round(x);
 
 
+	// Returns the value 20
+	x = Math.round(20.49);
+
+
+	// Returns the value 21
+	x = Math.round(20.5);
+
+
+	// Returns the value -20
+	x = Math.round(-20.5);
+
+
+	// Returns the value -21
+	x = Math.round(-20.51);
+
+
+	// Returns the value 1 (!)
+	// Note the rounding error because of inaccurate floating point arithmetics
+	// Compare this with Math.round10(1.005, -2) from the example below
 
 	document.write( Math.round(1.005 * 100) / 100); // => 1
 	
