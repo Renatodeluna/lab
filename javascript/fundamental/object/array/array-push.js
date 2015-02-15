1bc08f87caf973a6e7708e218e18b6b77232dec1
Renatodeluna <deluna.developer@gmail.com>
2015-02-14 23:55:18 -0300

    array push

---
+++ javascript/fundamental/object/array/array-push.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//  ==========================================================================
+//  Array Push
+//  ==========================================================================
+
+	
+
+	// Syntax
+	array.push(item1, item2, ..., itemN);
+
+
+	// Simple
+	var fruits = ["Banana", "Orange", "Apple", "Mango"];
+	fruits.push("Kiwi", "Lemon", "Pineapple");
+	// result: [Banana", "Orange", "Apple", "Mango", "Kiwi", "Lemon", "Pineapple"];
