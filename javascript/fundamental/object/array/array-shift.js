64bb12c373e86658f2a6716b2c3bf69c46723bb1
Renatodeluna <deluna.developer@gmail.com>
2015-02-15 01:24:03 -0300

    array shift

--- javascript/fundamental/object/array/array-shift.js
+++ javascript/fundamental/object/array/array-shift.js
@@ -11,4 +11,22 @@
 
 
 	// Simple
-	
+	var myList = ["angel", "home", "medical"];
+
+	console.log("mylist before: " + mylist);
+
+	var shifted = myList.shift();
+
+	console.log("myList after: " + myList); 
+	console.log("element remove:" + shifted);
+
+	// return:
+		// myList before: angel, home, medical
+		// myList after: home, medical
+		// Element remove: angel
+
+
+	var data = [1, 2, 3, 4];
+
+	console.log(data.shift());
+	console.log(data); // return: 2, 3, 4
