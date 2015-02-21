5193c392f691849d6c2dd494765a966eb013b10c
Renatodeluna <deluna.developer@gmail.com>
2015-02-21 02:05:04 -0300

    string toLowerCase

---
+++ javascript/fundamental/string/string-tolowercase.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//  ==========================================================================
+//  Array ToLowerCase
+//  ==========================================================================
+
+	
+
+	// Syntax
+	str.toLowerCase();
+
+
+	// Simple 
+	console.log("HELLO".toLowerCase());
+	//	return "hello"
+
+	var hi = "HELLO WORD!";
+	console.log(hi.toLowerCase());
+	//	return "hello word!"
