576645904d8ec81da068196452a646ce5fcfae73
Renatodeluna <deluna.developer@gmail.com>
2015-02-21 01:59:08 -0300

    string toUpperCase

---
+++ javascript/fundamental/string/string-touppercase.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//  ==========================================================================
+//  Array UpperCase
+//  ==========================================================================
+
+	
+
+	// Syntax
+	str.toUpperCase();
+
+
+	// Simple 
+	console.log("Hello Word!".toUpperCase());
+	//	return "HELLO WORD!"
+
+	var hi = "hello worde!";
+	console.log(hi.toUpperCase());
+	//	return "HELLO WORD!"
