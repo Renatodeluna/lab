28c8774a29ffc301e6e4fb0f0a81c41f2ca84fbd
Renatodeluna <deluna.developer@gmail.com>
2015-02-25 01:16:22 -0300

    date getFullYear

---
+++ javascript/fundamental/object/date/date-getfullyear.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//  ==========================================================================
+//  Date getFullYear
+//  ==========================================================================
+
+	
+
+	// Syntax
+	dateObj.getFullYear();
+
+
+	// Simple
+	var year = new Date();
+	console.log(year.getFullYear());
+	// return: 2015
+
+
+	var year = new Date("July 21, 1980 01:15:00");
+	console.log(year.getFullYear());
+	// return: 1980
