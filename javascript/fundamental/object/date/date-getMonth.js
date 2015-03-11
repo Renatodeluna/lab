8ddb49dd5a6a67d1625d9c198d33786dfd946d53
Renatodeluna <deluna.developer@gmail.com>
2015-03-10 23:56:06 -0300

    add date getMounth

--- javascript/fundamental/object/date/date-getMonth.js
+++ javascript/fundamental/object/date/date-getMonth.js
@@ -18,8 +18,37 @@
 	// return 2
 
 
-	var Xmas95 = new Date('December 25');
+	var Xmas95 = new Date('December 25, 1995 23:15:30');
 	var month = Xmas95.getMonth();
 
 	console.log(month);
 	// return 11
+
+
+	var d = new Date();
+	var month = new Array();
+
+	month[0]  = "January";
+	month[1]  = "February";
+	month[2]  = "March";
+	month[3]  = "April";
+	month[4]  = "May";
+	month[5]  = "June";
+	month[6]  = "July";
+	month[7]  = "August";
+	month[8]  = "September";
+	month[9]  = "October";
+	month[10] = "November";
+	month[11] = "December";
+
+	var n = month[d.getMonth()];
+
+	console.log(n);
+
+	console.log(d.getMonth());
+
+	// return March
+
+
+
+
