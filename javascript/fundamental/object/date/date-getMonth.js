4ac8917c42806180916d2ae37b38071ebb4d5b2a
Renatodeluna <deluna.developer@gmail.com>
2015-03-10 23:48:33 -0300

    date getMonth

---
+++ javascript/fundamental/object/date/date-getMonth.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//  ==========================================================================
+//  Date getMonth
+//  ==========================================================================
+
+	
+
+	// Syntax
+	//dateObj.getMonth()
+
+
+	// Simple
+	var d = new Date();
+	var n = d.getMonth();
+
+	console.log(n);
+	// return 2
+
+
+	var Xmas95 = new Date('December 25');
+	var month = Xmas95.getMonth();
+
+	console.log(month);
+	// return 11
